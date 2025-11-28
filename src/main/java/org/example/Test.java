9115ee092eb772e5f78c99dcf5ff25fe6ee05ba3
Sergey Vasilyev <vasilyevsv@e-ngineers.com>
2025-11-29 00:39:58 +0300

    Тест очереди для проекта esb

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -18,8 +18,8 @@ public class Test {
         Properties properties = new Properties();
         properties.put("java.naming.factory.initial", "org.apache.activemq.artemis.jndi.ActiveMQInitialContextFactory");
         properties.put("connectionFactory.jms/TestJms/MainCF",
-                "tcp://jms-0.artemis.esb.df.cloud:62627?type=QUEUE_CF&sslEnabled=false&clientID=TestJMS");
-        properties.put("client.fido_adapter.in", "in");
+                "tcp://jms-0.artemis.esb.df.cloud:443?type=QUEUE_CF&sslEnabled=true&clientID=TestJMS&trustAll=true");
+        properties.put("queue.jms/TestJms/in", "client.fido_adapter.in");
 
         QueueConnectionFactory queueConnectionFactory = null;
         Queue queue = null;
@@ -27,8 +27,8 @@ public class Test {
         try {
 
             Context initialContext = new InitialContext(properties);
-            queueConnectionFactory = (QueueConnectionFactory) initialContext.lookup("connectionFactory.jms/TestJms/MainCF");
-            queue = (Queue) initialContext.lookup("in");
+            queueConnectionFactory = (QueueConnectionFactory) initialContext.lookup("jms/TestJms/MainCF");
+            queue = (Queue) initialContext.lookup("jms/TestJms/in");
         } catch (Exception e) {
             throw new RuntimeException("Can't lookup administrative objects because of " + e.getClass().getSimpleName() + ": " + e.getMessage(), e);
         }
@@ -38,7 +38,7 @@ public class Test {
                 Session session = queueConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
                 MessageProducer messageProducer = session.createProducer(queue);
         ) {
-            String xml = "...";
+            String xml = "tru tu tu";
             TextMessage textMessage = session.createTextMessage(xml);
             messageProducer.send(textMessage);
         } catch (Exception e) {

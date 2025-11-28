bbf9ef3994133701b17d45a37bf129d107f29f0f
Sergey Vasilyev <vasilyevsv@e-ngineers.com>
2025-11-28 16:26:39 +0300

    Тест очереди для проекта esb

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -13,13 +13,13 @@ import javax.naming.InitialContext;
 
 public class Test {
     public static void main(String[] args) {
-        System.setProperty("javax.net.ssl.trustStore","truststore.jks");
+//        System.setProperty("javax.net.ssl.trustStore","truststore.jks");
 
         Properties properties = new Properties();
         properties.put("java.naming.factory.initial", "org.apache.activemq.artemis.jndi.ActiveMQInitialContextFactory");
-        properties.put("connectionFactory.jms/flexcube/OutboundEventArtemisQueueConnectionFactory",
-                "tcp://uz-is.dafr.loc:61616?type=QUEUE_CF&sslEnabled=true&clientID=FlexCube");
-        properties.put("queue.jms/flexcube/OutboundEventArtemisQueue", "FcOutboundEventQueue");
+        properties.put("connectionFactory.jms/TestJms/MainCF",
+                "tcp://jms-0.artemis.esb.df.cloud:62627?type=QUEUE_CF&sslEnabled=false&clientID=TestJMS");
+        properties.put("client.fido_adapter.in", "in");
 
         QueueConnectionFactory queueConnectionFactory = null;
         Queue queue = null;
@@ -27,14 +27,14 @@ public class Test {
         try {
 
             Context initialContext = new InitialContext(properties);
-            queueConnectionFactory = (QueueConnectionFactory) initialContext.lookup("jms/flexcube/OutboundEventArtemisQueueConnectionFactory");
-            queue = (Queue) initialContext.lookup("jms/flexcube/OutboundEventArtemisQueue");
+            queueConnectionFactory = (QueueConnectionFactory) initialContext.lookup("connectionFactory.jms/TestJms/MainCF");
+            queue = (Queue) initialContext.lookup("in");
         } catch (Exception e) {
             throw new RuntimeException("Can't lookup administrative objects because of " + e.getClass().getSimpleName() + ": " + e.getMessage(), e);
         }
 
         try (
-                QueueConnection queueConnection = queueConnectionFactory.createQueueConnection("flexcube", "flexcube");
+                QueueConnection queueConnection = queueConnectionFactory.createQueueConnection("admin", "admin!");
                 Session session = queueConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
                 MessageProducer messageProducer = session.createProducer(queue);
         ) {
